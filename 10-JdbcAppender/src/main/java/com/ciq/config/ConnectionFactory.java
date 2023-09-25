92e8e45e079fb8fd941c83cbc9763312bb9db892
91951 <91951@Laxman>
2023-09-25 10:46:11 +0530

    Jdbc Appender

--- 10-JdbcAppender/src/main/java/com/ciq/config/ConnectionFactory.java
+++ 10-JdbcAppender/src/main/java/com/ciq/config/ConnectionFactory.java
@@ -13,6 +13,7 @@ import org.apache.commons.dbcp.PoolingDataSource;
 import org.apache.commons.pool.impl.GenericObjectPool;
 
 public class ConnectionFactory {
+	
 	  private static interface Singleton {
 	        final ConnectionFactory INSTANCE = new ConnectionFactory();
 	    }
@@ -26,17 +27,14 @@ public class ConnectionFactory {
 
 	        GenericObjectPool<PoolableConnection> pool = new GenericObjectPool<PoolableConnection>();
 	        DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
-	                "jdbc:mysql://localhost:3306/myblog", properties
-	        );
+	                "jdbc:mysql://localhost:3306/myblog", properties);
 	        new PoolableConnectionFactory(
 	                connectionFactory, pool, null, "SELECT 1", 3, false, false, Connection.TRANSACTION_READ_COMMITTED
 	        );
-
 	        this.dataSource = new PoolingDataSource(pool);
 	    }
 
 	    public static Connection getDatabaseConnection() throws SQLException {
 	        return Singleton.INSTANCE.dataSource.getConnection();
-	    }
-
+	    } 
 }
